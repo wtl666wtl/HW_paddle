78a3e7af44640d0d2674a97dd48f51c5840e7517
wtl666wtl <2351090351@qq.com>
2022-01-08 15:46:51 +0800

    1

--- src/utils/config.py
+++ src/utils/config.py
@@ -62,6 +62,17 @@ __C.PCA.VOTING_ALPHA = 200.
 __C.PCA.GNN_LAYER = 5
 __C.PCA.GNN_FEAT = 1024
 
+# CIE model options
+__C.CIE = edict()
+__C.CIE.FEATURE_CHANNEL = 512
+__C.CIE.SK_ITER_NUM = 20
+__C.CIE.SK_EPSILON = 1.0e-10
+__C.CIE.SK_TAU = 0.005
+__C.CIE.GNN_LAYER = 5
+__C.CIE.GNN_FEAT = 1024
+__C.CIE.CROSS_ITER = False
+
+
 #
 # Training options
 #
